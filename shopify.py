e4f2857b6eae82631ebe6a825f1229e6241ff1c1
MoriartyJam <49534498+MoriartyJam@users.noreply.github.com>
2024-07-22 22:20:04 +0300

    Update shopify.py
    Add function for variables

--- shopify.py
+++ shopify.py
@@ -32,8 +32,6 @@ def fetch_product_balance():
         return response.json()
     return None
 
-
-
 def get_variant_id_by_sku(sku):
     print(f"Searching for SKU: {sku}")
     url = f'{shopify_store_url}/products.json'
@@ -49,7 +47,6 @@ def get_variant_id_by_sku(sku):
                 for variant in product['variants']:
                     if variant['sku'] == sku:
                         return variant['id']
-            # Обработка пагинации
             links = response.headers.get('Link', None)
             if links:
                 next_link = [link.split(';')[0].strip('<>') for link in links.split(',') if 'rel="next"' in link]
@@ -59,53 +56,6 @@ def get_variant_id_by_sku(sku):
         else:
             break
     return None
-
-
-def get_inventory_item_id(variant_id):
-    print(f"Fetching inventory item ID for variant: {variant_id}")
-    url = f'{shopify_store_url}/variants/{variant_id}.json'
-    headers = {
-        'Content-Type': 'application/json',
-        'X-Shopify-Access-Token': access_token
-    }
-    response = robust_request(url, method='get', headers=headers)
-    if response and response.status_code == 200:
-        return response.json()['variant']['inventory_item_id']
-    return None
-
-
-def update_shopify_inventory():
-    print("Updating Shopify inventory...")
-    product_data = fetch_product_balance()
-    if product_data:
-        for item in product_data:
-            if item['vendorCode'] and item['count'] is not None:  # Убедимся, что count существует
-                sku = item['vendorCode']
-                variant_id = get_variant_id_by_sku(sku)
-                if variant_id:
-                    inventory_item_id = get_inventory_item_id(variant_id)
-                    if inventory_item_id:
-                        count = max(0, floor(item['count']))  # Используем max для обеспечения неотрицательного значения
-                        url = f'{shopify_store_url}/inventory_levels/set.json'
-                        data = {
-                            'location_id': 89053102423,
-                            'inventory_item_id': inventory_item_id,
-                            'available': count
-                        }
-                        headers = {
-                            'Content-Type': 'application/json',
-                            'X-Shopify-Access-Token': access_token
-                        }
-                        response = robust_request(url, method='post', headers=headers, data=data)
-                        if response and response.status_code == 200:
-                            print(f"Successfully updated SKU {sku} with count {count}")
-                        else:
-                            print(f"Failed to update SKU {sku}: {response.status_code} {response.text}")
-                    else:
-                        print(f"Failed to find inventory item for SKU {sku}")
-                else:
-                    print(f"SKU {sku} not found in Shopify.")
-
 
 def robust_request(url, method='get', headers=None, data=None, params=None, retries=5, backoff_factor=0.3):
     print(f"Requesting {url} with method {method}")
@@ -126,13 +76,11 @@ def robust_request(url, method='get', headers=None, data=None, params=None, retr
     print("Max retries exceeded, request failed.")
     return None  # or raise an exception for critical processes
 
-
 scheduler = BackgroundScheduler()
-scheduler.add_job(update_shopify_inventory, 'interval', minutes=5)
+scheduler.add_job(update_shopify_inventory, 'interval', minutes=120)
 scheduler.start()
 
 try:
-
     while True:
         time.sleep(60)
 except (KeyboardInterrupt, SystemExit):
